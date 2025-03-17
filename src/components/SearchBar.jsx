56cc6ca93b62193295937e9fdd0905f5efc40a11
yousef yahia <yoousefyahia@gmail.com>
2025-03-17 05:54:15 +0200

    fix SearBar2

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { FaSearch } from "react-icons/fa";
-import "../styles/SearchBar";
+import "../styles/SearchBar.css";
 
 const SearchBar = ({ SearchArea, setSearchArea }) => {
   return (
